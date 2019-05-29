37679666fd7f0389e61a217e7482286ee32c3d63
reenanbs <reenanbsouza@gmail.com>
2019-05-28 21:13:47 -0300

    Inserção de Login do User e relacionamento de User e Contact

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,17 @@ class UsersController < ApplicationController
 
   # receberá um post para a criação do user
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Usuário cadastrado com sucesso'
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :name, :password, :password_confirmation)
+    end
 end
